d1d9a7fef670d2c3ca5c118e05958d68e0ebccfd
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2026-06-08 22:56:52 +0530

    Remove unused extension method

--- src/OpenFga.KubeOps/Extensions/ConditionExtensions.cs
+++ src/OpenFga.KubeOps/Extensions/ConditionExtensions.cs
@@ -4,32 +4,6 @@ namespace OpenFga.KubeOps.Extensions;
 
 public static class ConditionExtensions
 {
-    extension(IList<V1Condition> conditions)
-    {
-        public void SetCondition(string type, string status, string reason, string message)
-        {
-            var existingCondition = conditions.FirstOrDefault(c => c.Type == type);
-            if (existingCondition != null)
-            {
-                existingCondition.Status = status;
-                existingCondition.Reason = reason;
-                existingCondition.Message = message;
-                existingCondition.LastTransitionTime = DateTime.UtcNow;
-            }
-            else
-            {
-                conditions.Add(new V1Condition
-                {
-                    Type = type,
-                    Status = status,
-                    Reason = reason,
-                    Message = message,
-                    LastTransitionTime = DateTime.UtcNow
-                });
-            }
-        }
-    }
-
     extension(V1Condition)
     {
         public static V1Condition New(string type, string status, string reason, string message) => new()
