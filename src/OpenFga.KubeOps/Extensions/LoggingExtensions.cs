4f5414edbf8e30ab7c6ea9011c719763f835d360
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2026-06-12 18:59:01 +0530

    Log using text output template in Development environment

--- src/OpenFga.KubeOps/Extensions/LoggingExtensions.cs
+++ src/OpenFga.KubeOps/Extensions/LoggingExtensions.cs
@@ -7,17 +7,28 @@ namespace OpenFga.KubeOps.Extensions;
 
 public static class LoggingExtensions
 {
+    private const string DevelopmentOutputTemplate =
+        "[{Timestamp:HH:mm:ss.fff} {Level:u3}] <{SourceContext}> {Message:lj}{NewLine}{Exception}";
+
     extension(IServiceCollection services)
     {
-        public IServiceCollection AddSerilog(IConfiguration configuration)
+        public IServiceCollection AddSerilog(IConfiguration configuration, bool isDevelopment = false)
         {
             services.AddSerilog((provider, logger) =>
             {
                 logger
                     .ReadFrom.Configuration(configuration)
                     .ReadFrom.Services(provider)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console(new RenderedCompactJsonFormatter());
+                    .Enrich.FromLogContext();
+
+                if (isDevelopment)
+                {
+                    logger.WriteTo.Console(outputTemplate: DevelopmentOutputTemplate);
+                }
+                else
+                {
+                    logger.WriteTo.Console(new RenderedCompactJsonFormatter());
+                }
             });
             return services;
         }
