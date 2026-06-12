754e26d24f4dd264d740db868c357c329441f00c
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2026-06-12 16:54:50 +0530

    Implement FgaCliModelTranformer

--- src/OpenFga.KubeOps/Services/ModelTransformers/FgaCliModelTransformer.cs
+++ src/OpenFga.KubeOps/Services/ModelTransformers/FgaCliModelTransformer.cs
@@ -1,11 +1,35 @@
 using OpenFga.KubeOps.Abstractions;
+using System.Diagnostics;
 
 namespace OpenFga.KubeOps.Services.ModelTransformers;
 
-public class FgaCliModelTransformer : IModelTransformer
+public sealed class FgaCliModelTransformer : IModelTransformer
 {
-    public Task<string> TransformDslToJsonAsync(string modelDsl)
+    public async Task<string> TransformDslToJsonAsync(string modelDsl)
     {
-        return Task.FromResult("{\"sample\": \"Placeholder\"}");
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "/usr/local/bin/fga",
+            Arguments = $"model transform '{modelDsl}' --input-format=fga --output-format=json",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        using var process = new Process { StartInfo = processStartInfo };
+        process.Start();
+
+        var output = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode != 0)
+        {
+            throw new ModelTransformationFailedException(error);
+        }
+
+        return output;
     }
 }
